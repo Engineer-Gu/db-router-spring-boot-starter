19b462414712969bb6a8fba178ed3171f72c3fe1
laogu <1437594522@qq.com>
2023-02-24 22:56:42 +0800

    数据源基础配置

---
+++ src/main/java/com/lovegu/middleware/db/router/DBRouterBase.java
@@ -0,0 +1,19 @@
+package com.lovegu.middleware.db.router;
+
+/**
+ * @author 老顾
+ * @description 数据源基础配置
+ * @date 2023/2/24
+ */
+public class DBRouterBase {
+
+    private String tbIdx;
+
+    public String getTbIdx() {
+        return tbIdx;
+    }
+
+    public void setTbIdx(String tbIdx) {
+        this.tbIdx = tbIdx;
+    }
+}
